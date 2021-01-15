9cb3eb1c272ec4894dfa24c9f32a8f08eef3f71c
Alexey Makano <almakano@gmail.com>
2021-01-15 20:46:20 +0200

    Update ManagerServiceProvider.php

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -12,24 +12,14 @@ class ManagerServiceProvider extends ServiceProvider
 
 	public function boot(\Illuminate\Routing\Router $router)
 	{
-		$this->setupRoutes($this->app->router);
-		$this->publishFiles();
-	}
-
-	public function register() {
-
-		$this->commands($this->commands);
-	}
-
-	public function publishFiles()
-	{
+		$this->loadRoutesFrom(__DIR__.'/routes/botsmanager.php');
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'botsmanager');
 		$this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/botsmanager')]);
 		$this->publishes([__DIR__.'/../database/migrations/' => database_path('/migrations')]);
 	}
 
-	public function setupRoutes(\Illuminate\Routing\Router $router)
-	{
-		$this->loadRoutesFrom(__DIR__.'/routes/botsmanager.php');
+	public function register() {
+
+		$this->commands($this->commands);
 	}
 }

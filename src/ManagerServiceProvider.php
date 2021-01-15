ec42d72bbfc4e67385b1817449e50ba19832b628
Tester <tester@makano.pp.ua>
2021-01-15 19:38:18 +0200

    fix commands namespace path, try new database publish alias

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -7,23 +7,28 @@ use Illuminate\Support\ServiceProvider;
 class ManagerServiceProvider extends ServiceProvider
 {
 	protected $commands = [
-        \almakano\botsmanager\app\Console\Commands\Install::class,
-    ];
+		app\Console\Commands\Install::class,
+	];
 
 	public function boot(\Illuminate\Routing\Router $router)
-    {
-        $this->setupRoutes($this->app->router);
-        $this->publishFiles();
-    }
+	{
+		$this->setupRoutes($this->app->router);
+		$this->publishFiles();
+	}
 
-    public function publishFiles()
-    {
-    	$this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/almakano/botsmanager')], 'views');
-    	$this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')]);
-    }
+	public function register() {
 
-    public function setupRoutes(\Illuminate\Routing\Router $router)
-    {
-    	$this->loadRoutesFrom(__DIR__.'/routes/botsmanager.php');
-    }
+		$this->commands($this->commands);
+	}
+
+	public function publishFiles()
+	{
+		$this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/almakano/botsmanager')], 'views');
+		$this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'migrations');
+	}
+
+	public function setupRoutes(\Illuminate\Routing\Router $router)
+	{
+		$this->loadRoutesFrom(__DIR__.'/routes/botsmanager.php');
+	}
 }

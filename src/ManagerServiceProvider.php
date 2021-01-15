625c90089899f3b450ae6745b23fdc0eb4a41bb7
Tester <tester@makano.pp.ua>
2021-01-15 19:04:54 +0200

    update indents

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -7,22 +7,23 @@ use Illuminate\Support\ServiceProvider;
 class ManagerServiceProvider extends ServiceProvider
 {
 	protected $commands = [
-        \almakano\botsmanager\app\Console\Commands\Install::class,
-    ];
+		\almakano\botsmanager\app\Console\Commands\Install::class,
+	];
 
 	public function boot(\Illuminate\Routing\Router $router)
-    {
-        $this->setupRoutes($this->app->router);
-        $this->publishFiles();
-    }
+	{
+		$this->setupRoutes($this->app->router);
+		$this->publishFiles();
+	}
 
-    public function publishFiles()
-    {
-    	$this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/almakano/botsmanager')], 'views');
-    }
+	public function publishFiles()
+	{
+		$this->publishes([__DIR__.'/resources/views' => resource_path('views/vendor/almakano/botsmanager')], 'views');
+		$this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')]);
+	}
 
-    public function setupRoutes(Router $router)
-    {
-    	$this->loadRoutesFrom(base_path().__DIR__.'/routes/botsmanager.php');
-    }
+	public function setupRoutes(\Illuminate\Routing\Router $router)
+	{
+		$this->loadRoutesFrom(base_path().__DIR__.'/routes/botsmanager.php');
+	}
 }

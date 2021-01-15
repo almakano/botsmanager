ea4d146e487696f25b1954999166d48b994bef43
Tester <tester@makano.pp.ua>
2021-01-15 19:41:54 +0200

    add console install command name

--- src/app/Console/Commands/Install.php
+++ src/app/Console/Commands/Install.php
@@ -5,37 +5,40 @@ use Illuminate\Console\Command;
 
 class Install extends Command
 {
+	use Traits\PrettyCommandOutput;
 
-    use Traits\PrettyCommandOutput;
+	protected $progressBar;
 
-    protected $progressBar;
+	protected $signature = 'botsmanager:install
+								{--timeout=300} : How many seconds to allow each process to run.
+								{--debug} : Show process output or not. Useful for debugging';
 
 	public function handle()
-    {
-        $this->progressBar = $this->output->createProgressBar(5);
-        $this->progressBar->minSecondsBetweenRedraws(0);
-        $this->progressBar->maxSecondsBetweenRedraws(120);
-        $this->progressBar->setRedrawFrequency(1);
+	{
+		$this->progressBar = $this->output->createProgressBar(5);
+		$this->progressBar->minSecondsBetweenRedraws(0);
+		$this->progressBar->maxSecondsBetweenRedraws(120);
+		$this->progressBar->setRedrawFrequency(1);
 
-        $this->progressBar->start();
+		$this->progressBar->start();
 
-        $this->info(' BotsManager installation started. Please wait...');
-        $this->progressBar->advance();
+		$this->info(' BotsManager installation started. Please wait...');
+		$this->progressBar->advance();
 
-        $this->line(' Publishing configs, views, js and css files');
-        $this->executeArtisanProcess('vendor:publish', [
-            '--provider' => 'almakano\botsmanager\ManagerServiceProvider',
-            '--tag' => 'minimum',
-        ]);
+		$this->line(' Publishing configs, views, js and css files');
+		$this->executeArtisanProcess('vendor:publish', [
+			'--provider' => 'almakano\botsmanager\ManagerServiceProvider',
+			'--tag' => 'minimum',
+		]);
 
-        $this->line(" Creating users table (using Laravel's default migration)");
-        $this->executeArtisanProcess('migrate');
+		$this->line(" Creating users table (using Laravel's default migration)");
+		$this->executeArtisanProcess('migrate');
 
-        $this->line(" Creating App\Http\Middleware\CheckIfAdmin.php");
-        $this->executeArtisanProcess('botsmanager:publish-middleware');
+		$this->line(" Creating App\Http\Middleware\CheckIfAdmin.php");
+		$this->executeArtisanProcess('botsmanager:publish-middleware');
 
-        $this->progressBar->finish();
-        $this->info(' BotsManager installation finished.');
-    }
+		$this->progressBar->finish();
+		$this->info(' BotsManager installation finished.');
+	}
 }
 ?>
